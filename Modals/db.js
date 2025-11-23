813a0b9964ab0429a2ed3b4ea44fc9494a0f6879
lavishchauhan5555 <lavishchauhan524@gmail.com>
2025-11-24 00:25:40 +0530

    initial commit

--- Modals/db.js
+++ Modals/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const db = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error", err);
